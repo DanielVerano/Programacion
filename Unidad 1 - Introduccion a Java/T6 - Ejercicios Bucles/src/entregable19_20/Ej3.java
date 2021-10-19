c573be7096e999869106f954fb851b8e44f88b87
Daniel Verano <alum.dveranom.2021@iesalixar.org>
2021-10-19 21:07:39 +0200

    Correcciones en casa

--- Unidad 1 - Introduccion a Java/T6 - Ejercicios Bucles/src/entregable19_20/Ej3.java
+++ Unidad 1 - Introduccion a Java/T6 - Ejercicios Bucles/src/entregable19_20/Ej3.java
@@ -6,11 +6,27 @@ public class Ej3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numero;
+        String numBinarioManual = "";
 
         System.out.println("Introduce un número:");
         numero = sc.nextInt();
+        System.out.println("Número original: " + numero);
 
-        String numBinario = Integer.toBinaryString(numero);
-        System.out.println(numBinario);
+        while (numero > 0) {
+            if (numero % 2 == 0) {
+                numBinarioManual += "0";
+            } else {
+                numBinarioManual += "1";
+            }
+            numero /= 2;
+        }
+
+        System.out.print("Número en binario: ");
+        for (int i = numBinarioManual.length()-1; i >= 0; i--) {
+            System.out.print(numBinarioManual.charAt(i));
+        }
+
+//        String numBinario = Integer.toBinaryString(numero);
+//        System.out.println(numBinario);
     }
 }
--- Unidad 1 - Introduccion a Java/T6 - Ejercicios Bucles/src/entregable19_20/Ej5.java
+++ Unidad 1 - Introduccion a Java/T6 - Ejercicios Bucles/src/entregable19_20/Ej5.java
@@ -1,17 +1,23 @@
 package entregable19_20;
 
+import java.util.Scanner;
+
 public class Ej5 {
     public static void main(String[] args) {
-        String cadena = "SAMPLE";
-        int altura, anchura, numEsp, posPalabra;
+        int altura, anchura, numEsp;
+        Scanner sc = new Scanner(System.in);
 
-        /*if (cadena.length() > 10) {
-            cadena = cadena.substring(0, 11);
-        }*/
-        altura = 11;
+        System.out.println("Introduce una cadena:");
+        String cadena = sc.nextLine();
+
+        if (cadena.length() > 10) {
+            cadena = cadena.substring(0, 10);
+        }
+
+//        Altura del rombo: Longitud de la cadena * 2 - 1 (SAMPLE -> 6 * 2 - 1 = 11)
+        altura = 2 * cadena.length() - 1;
         anchura = altura;
-        numEsp = 5;
-        posPalabra = 0;
+        numEsp = cadena.length() - 1;
 
         for (int i = 0; i < altura; i++) {
             for (int j = 0; j < numEsp; j++) {
@@ -19,20 +25,13 @@ public class Ej5 {
             }
 
             for (int j = 0; j < anchura - 2 * numEsp; j++) {
+//                Imprimir primera mitad de la cadena (1.SAMPLE; 2.LPMAS)
                 if (j <= (anchura - 2 * numEsp) / 2) {
                     System.out.print(cadena.charAt(j));
-                    posPalabra = j;
-                    /*for (int k = 0; k < anchura / 2 + 1; k++) {
-                        System.out.print(cadena.charAt(k));
-                        posPalabra = k;
-                    }*/
                 } else {
-                    System.out.print(cadena.charAt(posPalabra-1));
-                    posPalabra--;
-                    /*for (int k = 0; k < anchura / 2; k++) {
-                        System.out.print(cadena.charAt(posPalabra-1));
-                        posPalabra++;
-                    }*/
+//                    Calcular posición a imprimir después de la mitad de la cadena
+//                    anchura (11) - j (6) - 1 = 4
+                    System.out.print(cadena.charAt(anchura - 2 * numEsp - j - 1));
                 }
             }
             System.out.println();
